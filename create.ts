70aa17beff0d8c2b7285d3980a035556e965fca3
b035 <marlon@erler.cc>
2023-02-06 16:24:17 +0100

    added subcommand checking

---
+++ create.ts
@@ -0,0 +1,5 @@
+import { Registry } from "@the-stations-project/sdk";
+
+export default function create(args: string[]) {
+	console.log(args);	
+}
--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
 #! /usr/bin/env node
 
-console.log("hello!");
+import create from "./create.js";
+
+function main(args: string[]) {
+	//remove first two arguments
+	args.splice(0, 2);
+
+	//get subcommand
+	const subcommand = args.splice(0, 1)[0];
+
+	switch (subcommand) {
+		case "create": return create(args);
+		case "remove": return;
+		case "get": return;
+		case "set": return;
+
+		default: throw `"${subcommand}": invalid subcommand.`;
+	}
+}
+
+main(process.argv);
